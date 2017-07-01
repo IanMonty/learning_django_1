25cf484a19be83406f46771ceb50c86288ee415a
Ian Montgomery <ianmonty86@gmail.com>
2017-06-30 18:20:10 -0700

    Front page HTML now generated from a template

--- function_tests.py
+++ function_tests.py
@@ -19,7 +19,6 @@ class NewVisitorTest(unittest.TestCase):
         # She notices the page title and header mention to-do list
         self.assertIn('To-Do', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
-        self.fail('Finished the test!')
         
         # She is invited to enter a to-do item straight away
         inputbox = self.browser.find_element_by_id('id_new_item')
@@ -34,13 +33,14 @@ class NewVisitorTest(unittest.TestCase):
         
         # When she hits enter. the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list
-        input.send_keys(Keys.ENTER)
+        inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
         
         table = self.browser.find_element_by_id('id_list_table')
-        row = table.find_elements_by_tag_name('tr')
+        rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows)
+            any(row.text == '1: Buy peacock feathers' for row in rows),
+            "New to-do item did not appear in table"
         )
         
         # There is still a text box inviting her to add another item. She
