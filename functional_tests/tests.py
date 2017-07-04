b1da7c5f75a2a153ad94dc563a93d6af414742b9
Ian Montgomery <ianmonty86@gmail.com>
2017-07-03 18:23:35 -0700

    first step of FT for layout + styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -97,7 +97,7 @@ class NewVisitorTest(LiveServerTestCase):
         # Francis starts a new list by entering a new item.  He
         # is less interesting than Edith...
         inputbox = self.browser.find_element_by_id('id_new_item')
-        inputbox.send_keys('Buy Milk')
+        inputbox.send_keys('Buy milk')
         inputbox.send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('1: Buy milk')
         
@@ -109,6 +109,31 @@ class NewVisitorTest(LiveServerTestCase):
         #Again, there is no trace of Edith's list
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
-        self.assertIn('Buy mild', page_text)
+        self.assertIn('Buy milk', page_text)
         
         #Satisfied, they both go back to sleep
+        
+    def test_layout_and_styling(self):
+        # Edith goes to the home page
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+        
+        # she notices the input box is nicely centered
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width']/2,
+            512,
+            delta=10
+        )
+        
+        # She starts a new list and sees the input is nicely
+        # cenerted there too
+        inputbox.send_keys('testing')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: testing')
+        input = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width']/2,
+            512,
+            delta=10
+        )
