05a43d1f1b1a4ad6fa29c12a1422a0f75f6c1994
Ian Montgomery <ianmonty86@gmail.com>
2017-07-05 17:42:14 -0700

    move sqlite database outside of main source tree

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -15,7 +15,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
         self.browser = webdriver.Firefox()
         staging_server = os.environ.get('STAGING_SERVER')
         if staging_server:
-            self.live_server_url - 'http://' + staging_server
+            self.live_server_url = 'http://' + staging_server
         
     def wait_for_row_in_list_table(self, row_text):
         start_time = time.time()
